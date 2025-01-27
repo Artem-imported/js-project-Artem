5c31ef75a9f8047d78c40c3c6c849020b6f5cf7c
Artem-imported <goreliiartemii261201@gmail.com>
2025-01-27 19:32:55 +0100

    7

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
+const welcomeModal = document.querySelector('.modal--welcome');
+const thankyouModal = document.querySelector('.modal--thankU');
+const closeWelcomeButton = document.querySelector('.close-welcome');
+const closeThankyouButton = document.querySelector('.close-thankU');
+const nameForm = document.querySelector('#name-form');
+
+function closeModal(modal) {
+    modal.classList.add('modal--hidden');
+}
+function showThankYou(event) {
+    event.preventDefault(); 
+    closeModal(welcomeModal);
+    thankyouModal.classList.remove('modal--hidden');
+}
+
+closeWelcomeButton.addEventListener('click', () => closeModal(welcomeModal));
+closeThankyouButton.addEventListener('click', () => closeModal(thankyouModal));
+nameForm.addEventListener('submit', showThankYou);
+
+const inputButton = document.getElementById('inputButton');
+const inputText = document.querySelector('.header__content--text');
+const input = document.getElementById('name-input')
+inputButton.addEventListener('click', () => {
+    inputText.textContent = `Вітаємо, ${input.value}`;
+})
+
+
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("submitBYear").addEventListener("click", () => {
         const year = parseInt(document.getElementById("bYear").value);
         const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
-        document.getElementById("bYearRes").textContent = isLeap ? "Це високосний рік" : "Це не високосний рік";
+        document.getElementById("bYearRes").textContent = isLeap ? "Ви народилися у високосний рік!" : "Ви народилися не у високосний рік!";
+        document.getElementById("bYearRes").style.color = isLeap ? "green" : "red";
     });
 
     let randNum = Math.floor(Math.random() * 10) + 1;
     document.getElementById("subRand").addEventListener("click", () => {
         const random = parseInt(document.getElementById("randNum").value);
-        const result = random === randNum ? "Ти вгадав!" : "Неправильно, спробуй ще!";
+        const result = random === randNum ? `Вітаю, ви вгадали число! (${randNum})` : `Ви програли, комп’ютер загадав ( ${randNum})`;
         document.getElementById("randRes").textContent = result;
         if (random !== randNum) randNum = Math.floor(Math.random() * 10) + 1;
     });
@@ -69,3 +97,9 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("maxRes").textContent = `Найбільше число: ${max}`;
     });
 });
+
+const footerInput = document.getElementById('footerInput');
+const footerBtn = document.getElementById('footerBtn');
+footerBtn.addEventListener('click', () => {
+    footerInput.value = 'Успішно!';
+})
